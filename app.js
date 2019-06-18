4ac8f18b307a72253724e09e0c5f774da744afa5
Avinash Jeyasankar <avinash.j@prodapt.com>
2019-06-18 19:54:06 +0530

    added config

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const debug = require('debug')('nodejs-assignment:app');
 const mongoose = require('mongoose');
+const dbURL = require('./config/db');
+const passport = require("passport");
 
-const db = mongoose.connect('mongodb://192.168.51.10:27017/nodeassignment', {
+const db = mongoose.connect(dbURL, {
   useNewUrlParser: true
 }).then(() => debug(`connected to MongoDB`)).catch((err) => debug(`Failed to connect to MongoDB`));
 mongoose.Promise = global.Promise;
 
 const User = require('./models/userModel');
-
-const passport = require("passport");
-// const passportJWT = require("passport-jwt");
 
 require('./auth/auth');
 
@@ -24,21 +23,13 @@ const type3Router = require('./routes/type3Router');
 const signupRouter = require('./routes/signupRouter')(User);
 const loginRouter = require('./routes/loginRouter')(User);
 
-
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-// app.use(cookieParser());
-
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/type1', passport.authenticate('jwt', {
   session: false
